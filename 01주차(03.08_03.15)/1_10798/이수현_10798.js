e5b5d520bb350fe0e75990b5a63a28ec15c20aad
suhyeon <suhyeon3484@naver.com>
2023-03-11 17:04:28 +0900

    [이수현] 1주차 10798번 문제풀이

---
+++ 01주차(03.08_03.15)/1_10798/이수현_10798.js
@@ -0,0 +1,17 @@
+// 10798 : 세로읽기
+// /dev/stdin
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const maxLength = Math.max(...input.map((i) => i.length));
+let answer = '';
+
+for (let i = 0; i < maxLength; i++) {
+  for (let j = 0; j < input.length; j++) {
+    answer += input[j][i] || '';
+  }
+}
+console.log(answer);
