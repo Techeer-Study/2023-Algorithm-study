32f15c559b8eaca6f7cf3c92854616d4c9fd9c16
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-03-15 21:24:03 +0900

    1주차 9506번 문제풀이

---
+++ 01주차(03.08_03.15)/2_9506/박희경_9506.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            int input = Integer.parseInt(br.readLine()); // int형으로 받기
+            int sum = 0;
+
+            if (input == -1)
+                break;
+
+            for (int i = 1; i < input; i++)
+                if (input % i == 0) {
+                    sb.append(i).append(" + ");
+                    sum += i;
+                }
+            System.out.println(input == sum ? input + " = " + sb.substring(0, sb.length() - 3) : input + " is NOT perfect.");
+        }
+    }
+}
