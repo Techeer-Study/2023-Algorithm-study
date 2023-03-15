5d5f317c0d31dd5c27ad1a5e43b319525886e85a
JungyeonKim <83697577+jung-yeon99@users.noreply.github.com>
2023-03-15 21:05:53 +0900

    Update 김정연_9506.js

--- 01주차(03.08_03.15)/2_9506/김정연_9506.js
+++ 01주차(03.08_03.15)/2_9506/김정연_9506.js
@@ -2,22 +2,22 @@ var fs = require("fs")
 var input = fs.readFileSync("/dev/stdin").toString().split("\n")
 
 let answer = ''
-let sum = 0;
-let index = 0;	
-let array = new Array();
-    for(let i = 1; i <= input.length; i++) {
-        if(input[i] == -1) break;
+for(let i = 0; i <= input.length; i++) { //input 받은걸 순서대로 나열
+    let sum = 0;
+    let array = new Array(); //약수를 찾아서 넣는 배열
+    if(input[i] == '-1') break; // input 값 중 -1 이 있다면 종료
+    for(let k = 1; k < input[i]; k++) { // 본인을 포함하지 않는 약수를 찾는 반복문, k = 약수
+        if(input[i] % k == 0) {	
+            array[i] = k; //약수를 배열에 저장
+            sum += k; // 약수들을 더한거를 사용자가 입력한 값과 비교하기위해
+        }
     }
-    for(let k = 1; k < input[i]; k++) {
-		if(input[i] % k == 0) {	
-            array[i] = k;
-            sum += i;
-    }
+
     if(sum != input[i]) {
-		    answer += input[i] + ' is NOT perfect.';
-	}
+        answer += input[i] + ' is NOT perfect.';
+    } //완전수 확인하는 조건문
     else {
-            answer += input[i] + ' = ' + array.join(' + ');
-    }
+        answer += input[i] + ' = ' + array.join(' + ');
+    } //약수들을 확인하는 조건문 
     console.log(answer);
-   }
+}
