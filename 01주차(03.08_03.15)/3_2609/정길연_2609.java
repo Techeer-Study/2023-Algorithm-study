0f4f55a4a156fc6e714e34df92f4a4d4bd651e85
gilyeon00 <12venus08@naver.com>
2023-03-15 12:57:46 +0900

    1주차 2609번 문제풀이

--- 01주차(03.08_03.15)/3_2609/정길연_2609.java
+++ 01주차(03.08_03.15)/3_2609/정길연_2609.java
@@ -1,9 +1,6 @@
-package Algo_3_2609;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class 정길연_2609 {
     public static void main(String[] args) throws IOException {
---
+++ 01주차(03.08_03.15)/Algo_5_2750/정길연_2750.java
@@ -0,0 +1,7 @@
+package Algo_5_2750;
+
+public class 정길연_2750 {
+    public static void main(String[] args) {
+
+    }
+}
