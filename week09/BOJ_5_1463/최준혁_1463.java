04a36f6b7f8332e869efaf043631fd7d3bb11e96
hi-june <wnsgur735@gmail.com>
2023-05-10 17:08:36 +0900

    9주차 1463번 문제풀이

---
+++ week09/BOJ_5_1463/최준혁_1463.java
@@ -0,0 +1,33 @@
+package week09.BOJ_5_1463;
+
+import java.io.*;
+
+class 최준혁_1463 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 2; i < n + 1; i++) {
+            // 현재 수에서 1을 빼는 경우
+            dp[i] = dp[i - 1] + 1;
+
+            // 현재 수가 2로 나누어 떨어지는 경우
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            // 현재 수가 3으로 나누어 떨어지는 경우
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+        bw.close();
+    }
+}
