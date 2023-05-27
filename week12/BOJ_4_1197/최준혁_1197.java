fb2ce14a7cd3ae5fe9532c7e26b8cb5174b66536
hi-june <wnsgur735@gmail.com>
2023-05-27 23:49:31 +0900

    12주차 1197번 문제풀이

--- week12/BOJ_4_1197/최준혁_1197.java
+++ week12/BOJ_4_1197/최준혁_1197.java
@@ -3,7 +3,7 @@ package week12.BOJ_4_1197;
 import java.io.*;
 import java.util.*;
 
-class Edge implements Comparable<Edge> {
+class Edge {
     private int distance;
     private int nodeA;
     private int nodeB;
@@ -25,39 +25,33 @@ class Edge implements Comparable<Edge> {
     public int getNodeB() {
         return this.nodeB;
     }
-
-    // 거리(비용)가 짧은 것이 높은 우선순위를 가지도록 설정
-    @Override
-    public int compareTo(Edge other) {
-        if (this.distance < other.distance) {
-            return -1;
-        }
-        return 1;
-    }
 }
 
 public class 최준혁_1197 {
-    // 노드의 개수(V)와 간선(Union 연산)의 개수(E)
-    // 노드의 개수는 최대 100,000개라고 가정
-    public static int v, e;
-    public static int[] parent = new int[100001]; // 부모 테이블 초기화하기
-    // 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
-    public static ArrayList<Edge> edges = new ArrayList<>();
+    public static int v, e; // 노드의 개수(V)와 간선(Union 연산)의 개수(E)
+    public static int[] parent = new int[100001]; // 부모 테이블 초기화하기(노드의 개수는 최대 100,000개라고 가정)
+    public static ArrayList<Edge> edges = new ArrayList<>();    // 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
     public static int result = 0;
 
     // 특정 원소가 속한 집합을 찾기
     public static int findParent(int x) {
-        // 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
-        if (x == parent[x]) return x;
-        return parent[x] = findParent(parent[x]);
+        if (x == parent[x]) {
+            return x;
+        }
+        parent[x] = findParent(parent[x]);  // 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+        return parent[x];
     }
 
     // 두 원소가 속한 집합을 합치기
     public static void unionParent(int a, int b) {
         a = findParent(a);
         b = findParent(b);
-        if (a < b) parent[b] = a;
-        else parent[a] = b;
+
+        if (a < b) {
+            parent[b] = a;
+        } else {
+            parent[a] = b;
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -77,22 +71,25 @@ public class 최준혁_1197 {
         // 모든 간선에 대한 정보를 입력 받기
         for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
+
             edges.add(new Edge(cost, a, b));
         }
 
         // 간선을 비용순으로 정렬
-        Collections.sort(edges);
+        Collections.sort(edges,
+                (e1, e2) -> e1.getDistance() - e2.getDistance());
 
         // 간선을 하나씩 확인하며
         for (int i = 0; i < edges.size(); i++) {
             int cost = edges.get(i).getDistance();
             int a = edges.get(i).getNodeA();
             int b = edges.get(i).getNodeB();
-            // 사이클이 발생하지 않는 경우에만 집합에 포함
-            if (findParent(a) != findParent(b)) {
+
+            if (findParent(a) != findParent(b)) {   // 사이클이 발생하지 않는 경우에만 집합에 포함
                 unionParent(a, b);
                 result += cost;
             }
