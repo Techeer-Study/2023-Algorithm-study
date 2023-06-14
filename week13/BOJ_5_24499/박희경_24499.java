d105b585b6e660d5bd574f3b304ec4eb8afff076
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-06-14 20:33:50 +0900

    13주차 24499번 문제풀이

---
+++ week13/BOJ_5_24499/박희경_24499.java
@@ -0,0 +1,37 @@
+package BOJ_5_24499;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 박희경_24499 {
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int [] degree = new int[N];
+
+        st = new StringTokenizer(br.readLine());;
+        for(int i = 0; i < N; i++) {
+            degree[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int [] sum = new int[N + 1];
+        for(int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + degree[i - 1];
+        }
+        int max = 0;
+
+        for(int i = 0; i < N; i++) {
+            if (i + K <= N)
+                max = Math.max(max, sum[i + K] - sum[i]);
+            else
+                max = Math.max(max, sum[N] - sum[i] + sum[(i + K) % N]);
+        }
+        System.out.println(max);
+    }
+}
