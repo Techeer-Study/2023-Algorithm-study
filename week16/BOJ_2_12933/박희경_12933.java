5bca24ae712f14ad1b9e1a08796c5c524069e03c
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-07-05 21:15:23 +0900

    16주차 12933번 문제풀이

---
+++ week16/BOJ_2_12933/박희경_12933.java
@@ -0,0 +1,39 @@
+package BOJ_2_12933;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 박희경_12933 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int[] arr = new int[6];
+        int size = str.length();
+        int max = 0;
+        arr[0] = size;
+
+        for(int i = 0; i < size; i++) {
+            int n = 0;
+            if(str.charAt(i) == 'q') n = 1;
+            if(str.charAt(i) == 'u') n = 2;
+            if(str.charAt(i) == 'a') n = 3;
+            if(str.charAt(i) == 'c') n = 4;
+            if(str.charAt(i) == 'k') n = 5;
+            if(arr[n - 1] == 0) {
+                System.out.println("-1");
+                return;
+            }
+            arr[n]++;
+            arr[n - 1]--;
+            max = Math.max(max, arr[1] + arr[2] + arr[3] + arr[4]);
+        }
+
+        if(arr[5] * 5 != size) {
+            System.out.println("-1");
+            return;
+        }
+        System.out.println(max);
+    }
+}
